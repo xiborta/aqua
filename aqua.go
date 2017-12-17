b691d86b22a47adadc6254f10d2402ceb43069d8
Luc Devallonné <luc.devallonne@gmail.com>
2017-12-17 21:08:47 +0100

    formatting go style

--- aqua.go
+++ aqua.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -9,20 +8,19 @@ import (
 	"github.com/dustin/go-coap"
 )
 
-
 func handleAqua(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
 
-	log.Printf("Reading: %s %v", strings.Join(m.Path(), "/"), string(m.Payload))	
-	
+	log.Printf("Reading: %s %v", strings.Join(m.Path(), "/"), string(m.Payload))
+
 	return nil
 }
 
 func main() {
-	
-  mux := coap.NewServeMux()
+
+	mux := coap.NewServeMux()
 	mux.Handle("/aqua/", coap.FuncHandler(handleAqua))
 
-  log.Printf("installing /aqua/ handler ...")
+	log.Printf("installing /aqua/ handler ...")
 
 	log.Fatal(coap.ListenAndServe("udp", ":5683", mux))
 }
